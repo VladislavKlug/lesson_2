dc272ba997b63e3d9768a639e8caa914914576ed
Vlad <v.clug@yandex.ru>
2022-02-13 16:56:19 +0300

    homework lesson_4

---
+++ index.js
@@ -0,0 +1,40 @@
+let str = 'string';
+let bool = true;
+let num = 10;
+
+let sum1 = str + bool;
+let sum2 = str + num;
+let sum3 = num + bool;
+
+let multiplic1 = str * bool;
+let multiplic2 = str * num;
+let multiplic3 = num * bool;
+
+let division1 = str / bool;
+let division2 = str / num;
+let division3 = num / bool;
+
+let value1 = String(100);
+let value2 = Boolean('Hello');
+let value3 = Number(true);
+
+console.log(sum1);
+console.log(sum2);
+console.log(sum3);
+
+console.log(multiplic1);
+console.log(multiplic2);
+console.log(multiplic3);
+
+console.log(division1);
+console.log(division2);
+console.log(division3);
+
+console.log(value1);
+console.log(typeof value1);
+
+console.log(value2);
+console.log(typeof value2);
+
+console.log(value3);
+console.log(typeof value3);
